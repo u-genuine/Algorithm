8579790986808e3791a79edbc3d931a18ba21cdc
Yujin Park <101379241+u-genuine@users.noreply.github.com>
2025-02-25 10:28:22 +0900

    [level 2] Title: 올바른 괄호, Time: 20.88 ms, Memory: 54.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,41 +1,18 @@
-/*
-큐에 다 집어넣고
-
-if(')'로 시작하면)
-    false
-else 
-    '(' 하나 뺄때마다 count+1
-    ')' 하나 뺄때마다 count-1
-    if(count < 0 )
-        false
-    다 뺐을때 count가 0이면 성공
-*/
+import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-        
-        char first = s.charAt(0);
-        int count = 0;
-        
-        if(first == ')')
-            answer = false;
-        
-        for(char i : s.toCharArray()) {
-            if(i == '(') {
-                count++;
-            } else {
-                count--;
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '('){
+                stack.push('(');
+            } else if(s.charAt(i) == ')') {
+                if(stack.isEmpty()){
+                    return false;
+                }
+                stack.pop();
             }
-            
-            if(count < 0)
-                answer = false;
         }
-        
-        if(count > 0) {
-            answer = false;
+            return stack.isEmpty();
         }
-        
-        return answer;
-    }
 }
