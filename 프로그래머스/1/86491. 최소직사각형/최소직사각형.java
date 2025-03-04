90daeceb590aad7dde41da2ca4ec79f580273b48
Yujin Park <101379241+u-genuine@users.noreply.github.com>
2025-03-04 10:31:37 +0900

    [level 1] Title: 최소직사각형, Time: 1.85 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,41 +1,15 @@
-/*
-각각의 원소에 대해서 가로, 세로 중 긴 것이 앞에 오도록 새 배열 생성
-
-새 배열에 대해서 
-a = 가로 중 가장 긴 것
-b = 세로 중 가장 긴 것
-
-return a*b;
-*/
-
-import java.util.*;
-
 class Solution {
     public int solution(int[][] sizes) {
+        int width = 0;
+        int height = 0;
         
-        int[][] sizes2 = new int[sizes.length][2];
-        
-        for (int i = 0; i < sizes.length; i++) {
-            if (sizes[i][0] >= sizes[i][1]){
-                sizes2[i] = sizes[i];
-            } else {
-                sizes2[i][0] = sizes[i][1];
-                sizes2[i][1] = sizes[i][0];
-            }
+        for(int[] card : sizes) {
+            // 가로, 세로 중 긴변이 width, 짧은 변이 height
+            // 모든 명함 중 가장 긴 width, height를 찾기
+            width = Math.max(width,  Math.max(card[0], card[1]));
+            height = Math.max(height, Math.min(card[0], card[1]));
         }
         
-        int maxW = 0;
-        int maxH = 0;
-        
-        for (int[] one : sizes2) {
-            if(one[0] > maxW) {
-                maxW = one[0];
-            }
-            if(one[1] > maxH) {
-                maxH = one[1];
-            }
-        }
-        
-        return maxW*maxH;
+        return width * height;
     }
 }
