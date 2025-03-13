90a44fa98c99f2608aaebffc7dfbeec412d2502f
Yujin Park <101379241+u-genuine@users.noreply.github.com>
2025-03-13 12:23:44 +0900

    [Silver IV] Title: 수 찾기, Time: 1636 ms, Memory: 186616 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,20 +1,36 @@
 import java.util.*;
 
-public class Main{
+class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        Set<Integer> setN = new HashSet<>();
+        int[] arrN = new int[N];
+  
         for(int i = 0; i < N; i++) {
-            setN.add(sc.nextInt());    
+            arrN[i] = sc.nextInt();
         }
+        
+        Arrays.sort(arrN);
         
         int M = sc.nextInt();
         for(int i = 0; i < M; i++){
-            if(setN.contains(sc.nextInt()))
-                System.out.println(1);
-            else
-                System.out.println(0);
+            int answer = binarySearch(arrN, 0, N-1, sc.nextInt());
+            System.out.println(answer);
         }
+        
+    }
+    
+    public static int binarySearch(int[] arr, int left, int right, int target){
+        while(left <= right){
+            int mid = (left + right) / 2;
+            if(arr[mid] < target){
+                left = mid + 1;
+            } else if(arr[mid] > target) {
+                right = mid - 1;
+            } else {
+                return 1;
+            }
+        }
+        return 0;
     }
 }
