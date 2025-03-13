5b1fc3dbbd9215b230df1b027f20a20d454cfaa5
Yujin Park <101379241+u-genuine@users.noreply.github.com>
2025-03-13 14:50:11 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 1248 ms, Memory: 440832 KB -BaekjoonHub

---
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+
+		Set<String> set = new HashSet<>();
+
+		for (int i = 0; i < str.length(); i++) {
+			for(int j = i + 1; j <= str.length(); j++) {
+				if(!set.contains(str.substring(i, j))){
+					set.add(str.substring(i, j));
+				}
+			}
+		}
+
+		System.out.println(set.size());
+	}
+}
