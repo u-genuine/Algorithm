dab339069fcfeb94dc98b4ac194003722bbec976
Yujin Park <101379241+u-genuine@users.noreply.github.com>
2025-04-09 13:23:32 +0900

    [Gold V] Title: A와 B, Time: 108 ms, Memory: 14660 KB -BaekjoonHub

---
+++ 백준/Gold/12904. A와 B/A와 B.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+class Main{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder S = new StringBuilder(br.readLine());
+		StringBuilder T = new StringBuilder(br.readLine());
+
+		while(S.length() < T.length()){
+			if(T.charAt(T.length() - 1) == 'A') {
+				T.deleteCharAt(T.length() - 1);
+			} else {
+				T.deleteCharAt(T.length() - 1);
+				T.reverse();
+			}
+		}
+		System.out.println(S.toString().equals(T.toString()) ? 1 : 0);
+	}
+}
