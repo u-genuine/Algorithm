bc814a6c554fab50a35e2e4e32f0303735c19e06
Yujin Park <101379241+u-genuine@users.noreply.github.com>
2025-04-30 13:11:55 +0900

    [Silver II] Title: N과 M (11), Time: 324 ms, Memory: 33616 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N;
+	static int M;
+	static int[] arr;
+	static int[] result;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		result = new int[M];
+
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr);
+
+		DFS(0);
+		System.out.println(sb);
+	}
+
+	static void DFS(int count) {
+		if(count == M) {
+			for(int i = 0; i < M; i++) {
+				sb.append(result[i]).append(' ');
+			}
+			sb.append("\n");
+			return;
+		}
+		int before = 0;
+		for(int i = 0; i < N; i++) {
+			if(before != arr[i]) {
+				result[count] = arr[i];
+				before = arr[i];
+				DFS(count + 1);
+			}
+		}
+	}
+}
