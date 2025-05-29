99c69fab44042b90f90c92f147b2fcda05341664
Yujin Park <101379241+u-genuine@users.noreply.github.com>
2025-05-29 13:53:43 +0900

    [level 2] Title: 타겟 넘버, Time: 59.14 ms, Memory: 94.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,28 +1,39 @@
+/*
+-로 한번 +로 한번?
+2^숫자개수 2^20 충분하네~
+*/
+
 class Solution {
-    int[] numbers;
-    int target;
-    int answer;
+    static int answer = 0;
     
     public int solution(int[] numbers, int target) {
-        this.numbers = numbers;
-        this.target = target;
         
-        answer = 0;
-        dfs(0, 0);
+        boolean[] plus = new boolean[numbers.length];
+        DFS(numbers, target, 0, plus);
+        
         return answer;
     }
     
-    public void dfs(int index, int sum){
-        // 1. 탈출 조건
-        if(index == numbers.length){
-            if (sum == target)
+    void DFS(int[] numbers, int target, int depth, boolean[] plus) {
+        if(depth == numbers.length) {
+            int result = 0;
+            for(int i = 0; i < numbers.length; i++) {
+                if(plus[i]) {
+                    result += numbers[i];
+                } else {
+                    result -= numbers[i];
+                }
+            }
+            
+            if(result == target) {
                 answer++;
+            }
             return;
         }
         
-        // 2. 수행 동작
-        dfs(index + 1, sum + numbers[index]);
-        dfs(index + 1, sum - numbers[index]);
-        
+        plus[depth] = true;
+        DFS(numbers, target, depth + 1, plus);
+        plus[depth] = false;
+        DFS(numbers, target, depth + 1, plus);
     }
 }
