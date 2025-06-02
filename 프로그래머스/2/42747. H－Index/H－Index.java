e58641371a518f34a3e03edb8b7ab85095bb1f2b
Yujin Park <101379241+u-genuine@users.noreply.github.com>
2025-06-02 14:12:44 +0900

    [level 2] Title: H-Index, Time: 1.23 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,20 +1,18 @@
-/*
-n개의 논문 중
-h번 이상 인용된 논문이 h편 이상인 h의 최댓값
-*/
 import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
-        int n = citations.length;
+        int answer = 0;
         
         Arrays.sort(citations);
-        
-        for(int i = 0; i < n; i++){
-            int h = n - i; // h: citations[i]번 이상 인용된 논문 개수
-            if(citations[i] >= h)
-                return h;
+
+        for(int i = 0; i < citations.length; i++) {
+            int h = citations.length - i;
+            if(citations[i] >= h) {
+                answer = h;
+                break;
+            }
         }
-        return 0;
+        return answer;
     }
 }
