f5e591e9f7017ecd3bc66b0948e608cf1b8e41fa
Yujin Park <101379241+u-genuine@users.noreply.github.com>
2025-06-02 14:43:37 +0900

    [level 2] Title: 가장 큰 수, Time: 3187.87 ms, Memory: 406 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,22 +1,27 @@
-import java.util.Arrays;
+import java.util.*;
+
 class Solution {
     public String solution(int[] numbers) {
-        // 숫자를 문자열로 반환
-        String[] strNums = new String[numbers.length];
-        for(int i = 0; i < numbers.length; i++){
-            strNums[i] = String.valueOf(numbers[i]);
+        String[] numberStr = new String[numbers.length];
+        
+        for(int i = 0; i < numbers.length; i++) {
+            String numStr = String.valueOf(numbers[i]);
+            numberStr[i] = numStr;
         }
         
-        // 두 수를 이어붙였을 때 더 큰 수가 되는 순서로 정렬
-        Arrays.sort(strNums, (a, b) -> (b + a).compareTo(a + b));
+        Arrays.sort(numberStr, (a, b) -> (a + b).compareTo(b + a));
         
-        // 가장 큰 수가 0이면 "0" 반환 (모든 수가 0인 경우)
-        if(strNums[0].equals("0")) {
+        if(numberStr[numberStr.length - 1].equals("0")) {
             return "0";
         }
         
-        // 정렬이 된 문자열 이어붙이기
-        String answer = String.join("", strNums);
+        String answer = "";
+        
+        for(int i = numberStr.length - 1; i >= 0; i--) {
+            answer += numberStr[i];
+        }
+        
+        
         return answer;
     }
 }
