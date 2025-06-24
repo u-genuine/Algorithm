cafc11ba9997e10204f165b45ab9ec03b2ba2aaa
Yujin Park <101379241+u-genuine@users.noreply.github.com>
2025-06-24 11:35:15 +0900

    [Gold IV] Title: 인구 이동, Time: 708 ms, Memory: 305740 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -2,58 +2,59 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int[] dr = new int[]{-1, 1, 0, 0};
-	static int[] dc = new int[]{0, 0, -1, 1};
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		int N = Integer.parseInt(st.nextToken());
-		int L = Integer.parseInt(st.nextToken());
-		int R = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken()); // L 이상
+		int R = Integer.parseInt(st.nextToken()); // R 이하
 
-		int[][] map = new int[N][N];
-		for(int n = 0; n < N; n++) {
+		int[][] nums = new int[N][N];
+
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int m = 0; m < N; m++) {
-				map[n][m] = Integer.parseInt(st.nextToken());
+			for (int j = 0; j < N; j++) {
+				nums[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 
 		int days = 0;
 
 		while(true) {
-			boolean[][] visit = new boolean[N][N];
+			boolean[][] visited = new boolean[N][N];
 			boolean moved = false;
 
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					if (visit[i][j])
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < N; j++) {
+					if(visited[i][j]) {
 						continue;
+					}
 
 					Queue<int[]> queue = new LinkedList<>();
-					List<int[]> union = new ArrayList<>();
-					queue.add(new int[] {i, j});
-					union.add(new int[] {i, j});
+					queue.add(new int[]{i, j});
+					List<int[]> union = new LinkedList<>();
+					union.add(new int[]{i, j});
 
-					visit[i][j] = true;
+					visited[i][j] = true;
 
-					while (!queue.isEmpty()) {
+					while(!queue.isEmpty()) {
 						int[] cur = queue.poll();
 
-						for (int k = 0; k < 4; k++) {
+						for(int k = 0; k < 4; k++) {
 							int nextR = cur[0] + dr[k];
 							int nextC = cur[1] + dc[k];
 
-							if (nextR >= 0 && nextR < N && nextC >= 0 && nextC < N) {
-								if (!visit[nextR][nextC]) {
-
-									int sub = Math.abs(map[cur[0]][cur[1]] - map[nextR][nextC]);
-									if (sub >= L && sub <= R) {
-                                        visit[nextR][nextC] = true;
-										queue.add(new int[] {nextR, nextC});
-										union.add(new int[] {nextR, nextC});
+							if(nextR >= 0 && nextR < N && nextC >= 0 && nextC < N) {
+								if(!visited[nextR][nextC]) {
+									int sub = Math.abs(nums[cur[0]][cur[1]] - nums[nextR][nextC]);
+									if(sub >= L && sub <= R) {
+										visited[nextR][nextC] = true;
+										queue.add(new int[]{nextR, nextC});
+										union.add(new int[]{nextR, nextC});
 									}
 								}
 							}
@@ -66,12 +67,12 @@ public class Main {
 						moved = true;
 
 						int sum = 0;
-						for (int[] one : union) {
-							sum += map[one[0]][one[1]];
+						for(int[] one : union) {
+							sum += nums[one[0]][one[1]];
 						}
 
-						for (int[] one : union) {
-							map[one[0]][one[1]] = sum / unionSize;
+						for(int[] one : union) {
+							nums[one[0]][one[1]] = sum/unionSize;
 						}
 					}
 				}
@@ -83,7 +84,7 @@ public class Main {
 
 			days++;
 		}
-
 		System.out.println(days);
+
 	}
 }
