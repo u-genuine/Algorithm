da251dcd781b3501da6b4c721394f0a430931c01
Yujin Park <101379241+u-genuine@users.noreply.github.com>
2025-06-26 12:01:40 +0900

    [Silver I] Title: 숨바꼭질, Time: 136 ms, Memory: 18664 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,41 +1,45 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
+public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		boolean[] visited = new boolean[1000001];
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
 
-		Queue<int[]> queue = new LinkedList<>();
-		queue.add(new int[] {N, 0});
+		int[] result = new int[100001];
+		result[n] = 0;
+		boolean[] visited = new boolean[100001];
 
-		while (!queue.isEmpty()) {
-			int[] current = queue.poll();
-			int X = current[0];
-			int sec = current[1];
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(n);
+		visited[n] = true;
 
-			if (X == K) {
-				System.out.println(sec);
-				return;
+		while(!queue.isEmpty()) {
+			int cur = queue.poll();
+
+			if(cur == k) {
+				System.out.println(result[cur]);
+				break;
 			}
-			if (visited[X] || X >= 200000) {
-				continue;
+
+			if(cur + 1 < 100001 && !visited[cur + 1]) {
+				queue.add(cur + 1);
+				result[cur + 1] = result[cur] + 1;
+				visited[cur + 1] = true;
 			}
-
-			int[] nextPos = {X - 1, X + 1, X * 2};
-
-			for (int next : nextPos) {
-				if (next < 0 || next > 100000 || visited[next]) {
-					continue;
-				}
-
-				visited[X] = true;
-				queue.add(new int[] {next, sec + 1});
+			if(cur - 1 >= 0 && !visited[cur - 1]) {
+				queue.add(cur - 1);
+				result[cur - 1] = result[cur] + 1;
+				visited[cur - 1] = true;
+			}
+			if(cur * 2 < 100001 && !visited[cur * 2]) {
+				queue.add(cur * 2);
+				result[cur * 2] = result[cur] + 1;
+				visited[cur * 2] = true;
 			}
 		}
-	}
+ 	}
 }
