949384b7cc0aa8160ad4268d673ba72105a43042
Yujin Park <101379241+u-genuine@users.noreply.github.com>
2025-06-27 11:18:41 +0900

    [Silver I] Title: 단축키 지정, Time: 104 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Silver/1283. 단축키 지정/단축키 지정.java
+++ 백준/Silver/1283. 단축키 지정/단축키 지정.java
@@ -5,72 +5,64 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+		Set<String> set = new HashSet<>();
+
 		int N = Integer.parseInt(br.readLine());
 		ArrayList<String>[] options = new ArrayList[N];
 		ArrayList<Integer>[] wordIdx = new ArrayList[N];
 
-		for(int i = 0; i < options.length; i++) {
-			options[i] = new ArrayList<>();
+		for(int i = 0; i < N; i++) {
 			String line = br.readLine();
-			for(String str : line.split("")) {
-				options[i].add(str);
-			}
-		}
-
-		for(int i = 0; i < options.length; i++) {
-			ArrayList<String> option = options[i];
-			int size = option.size();
-
+			options[i] = new ArrayList<>();
 			wordIdx[i] = new ArrayList<>();
-			wordIdx[i].add(0);
 
-			for(int j = 0; j < size; j++) {
-				if(option.get(j).equals(" ")) {
-					wordIdx[i].add(j + 1);
+			String[] str = line.split("");
+			wordIdx[i].add(0);
+			for(int j = 0; j < str.length; j++) {
+				if(str[j].equals(" ")) {
+					wordIdx[i].add(j+1);
 				}
+				options[i].add(str[j]);
 			}
 		}
 
-		Set<String> set = new HashSet<>();
-
-		for(int i = 0; i < options.length; i++) {
-			boolean find = false;
-			for(int idx : wordIdx[i]) {
-				if(!set.contains(options[i].get(idx).toLowerCase())) {
-					set.add(options[i].get(idx).toLowerCase());
-					options[i].add(idx, "[");
-					options[i].add(idx + 2, "]");
-					find = true;
+		for(int i = 0; i < N; i++) {
+			boolean check = false;
+			for(int j = 0; j < wordIdx[i].size(); j++) {
+				int targetIdx = wordIdx[i].get(j);
+				if(!set.contains(options[i].get(targetIdx).toLowerCase())) {
+					set.add(options[i].get(targetIdx).toLowerCase());
+					options[i].add(targetIdx, "[");
+					options[i].add(targetIdx + 2, "]");
+					check = true;
 					break;
 				}
 			}
 
-			if(find) {
-				continue;
-			}
-
-			for(int j = 0; j < options[i].size(); j++) {
-				if(options[i].get(j).equals(" "))
-					continue;
-				
-				if(!set.contains(options[i].get(j).toLowerCase())) {
-					set.add(options[i].get(j).toLowerCase());
-					options[i].add(j, "[");
-					options[i].add(j + 2, "]");
-					break;
+			if(!check) {
+				for(int j = 0; j < options[i].size(); j++) {
+					if(!set.contains(options[i].get(j).toLowerCase())) {
+						if(!options[i].get(j).equals(" ")) {
+							set.add(options[i].get(j).toLowerCase());
+							options[i].add(j, "[");
+							options[i].add(j + 2, "]");
+							break;
+						}
+					}
 				}
 			}
 		}
 
 		StringBuilder sb = new StringBuilder();
-		for(ArrayList<String> option : options) {
-			for(String s : option) {
-				sb.append(s);
+
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < options[i].size(); j++) {
+				sb.append(options[i].get(j));
 			}
 			sb.append("\n");
 		}
 
 		System.out.println(sb);
 
-	}
+ 	}
 }
