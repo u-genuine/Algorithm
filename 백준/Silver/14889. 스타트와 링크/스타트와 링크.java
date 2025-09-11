23406cba783083e073d48f970050ce8496cd525e
Yujin Park <101379241+u-genuine@users.noreply.github.com>
2025-09-11 11:52:42 +0900

    [Silver I] Title: 스타트와 링크, Time: 236 ms, Memory: 19132 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,62 +1,63 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-class Main{
-    static int N;
-    static int[][] map;
-    static boolean[] visited;
-    static int min = Integer.MAX_VALUE;
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        N = Integer.parseInt(br.readLine());
-        
-        map = new int[N+1][N+1];
-        visited = new boolean[N+1];
-        
-        for(int i = 1; i < N+1; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j = 1; j < N+1; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        
-        combi(0, 1);
-        System.out.println(min);
-    }
-    
-    static void combi(int count, int start) {
-        if(count == N/2){
-            min = Math.min(min, calculate());
-            return;
-        }
-        
-        for(int i = start; i < N+1; i++) {
-                visited[i] = true;
-                combi(count+1, i+1);
-                visited[i] = false;
-            
-        }
-    }
+public class Main {
+	static int N;
+	static int[][] S;
+	static int min = Integer.MAX_VALUE;
+	static boolean[] inStart;
 
-    static int calculate() {
-        int start = 0;
-        int link = 0;
-        
-        for(int i = 1; i < N+1; i++) {
-            for(int j = 1; j < N+1; j++) {
-                if(i == j) {
-                    continue;
-                }
-                if(visited[i] && visited[j]) {
-                    start += map[i][j];
-                }
-                if(!visited[i] && !visited[j]) {
-                    link += map[i][j];
-                }
-            }
-        }
-        return Math.abs(start-link);
-    }   
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		S = new int[N + 1][N + 1];
+		inStart = new boolean[N + 1];
+
+		for (int i = 1; i <= N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= N; j++) {
+				S[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		inStart[1] = true;
+		DFS(1, 2);
+
+		System.out.println(min);
+	}
+
+	static void DFS(int count, int idx) {
+		if (idx == N + 1) {
+			if (count == N/2) {
+				min = Math.min(min, calcS());
+			}
+			return;
+		}
+
+		inStart[idx] = true;
+		DFS(count + 1, idx + 1);
+		inStart[idx] = false;
+		DFS(count, idx + 1);
+	}
+
+	static int calcS() {
+		int start = 0;
+		int link = 0;
+
+		for(int i = 1; i <= N; i++) {
+			for(int j = 1; j <= N; j++) {
+				if(inStart[i]) {
+					if(inStart[j]) {
+						start += S[i][j];
+					}
+				} else {
+					if(!inStart[j]) {
+						link += S[i][j];
+					}
+				}
+			}
+		}
+
+		return Math.abs(start - link);
+	}
 }
