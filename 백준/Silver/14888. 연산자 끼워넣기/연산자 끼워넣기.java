cdaa0a2285117d9ae9ff96b4a4baf1ab573943b7
Yujin Park <101379241+u-genuine@users.noreply.github.com>
2025-09-11 11:05:27 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 104 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,62 +1,68 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
-	static int max = Integer.MIN_VALUE;
-	static int min = Integer.MAX_VALUE;
+public class Main {
 	static int N;
+	static int[] num;
 	static int[] operator = new int[4];
-	static int[] nums;
+	static long max = Long.MIN_VALUE;
+	static long min = Long.MAX_VALUE;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		N = Integer.parseInt(br.readLine());
+		num = new int[N];
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		nums = new int[N];
 		for(int i = 0; i < N; i++) {
-			nums[i] = Integer.parseInt(st.nextToken());
+			num[i] = Integer.parseInt(st.nextToken());
 		}
 
 		st = new StringTokenizer(br.readLine());
-		operator = new int[4];
 		for(int i = 0; i < 4; i++) {
 			operator[i] = Integer.parseInt(st.nextToken());
 		}
 
-		calc(nums[0], 1);
+		DFS(num[0], 1);
 		System.out.println(max);
 		System.out.println(min);
 	}
 
-	static void calc(int tempResult, int index) {
-		if(index == N) {
-			min = Math.min(min, tempResult);
-			max = Math.max(max, tempResult);
+	static void DFS(long curVal, int nextIdx) {
+		if(nextIdx == N) {
+			max = Math.max(max, curVal);
+			min = Math.min(min, curVal);
 			return;
 		}
 
-		for(int i = 0; i < 4; i++){
-			if(operator[i] == 0) {
-				continue;
+		for(int i = 0; i < 4; i++) {
+			if(operator[i] > 0) {
+				long nextValue = operate(curVal, num[nextIdx], i);
+				operator[i]--;
+				DFS(nextValue, nextIdx + 1);
+				operator[i]++;
 			}
-			operator[i]--;
-			switch(i) {
-				case 0:
-					calc(tempResult + nums[index], index + 1);
-					break;
-				case 1:
-					calc(tempResult - nums[index], index + 1);
-					break;
-				case 2:
-					calc(tempResult * nums[index], index + 1);
-					break;
-				case 3:
-					calc(tempResult / nums[index], index + 1);
-					break;
-			}
-			operator[i]++;
 		}
 	}
 
+	static long operate(long num1, long num2, int opIdx) {
+		long result = 0;
+		switch(opIdx) {
+			case 0:
+				result = num1 + num2;
+				break;
+			case 1:
+				result = num1 - num2;
+				break;
+			case 2:
+				result = num1 * num2;
+				break;
+			case 3:
+				result = num1 / num2;
+				break;
+		}
+
+		return result;
+	}
 }
