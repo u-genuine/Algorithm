baee9b3e1f0b1fea8ee04a9bafba15356a9b5408
Yujin Park <101379241+u-genuine@users.noreply.github.com>
2025-09-13 12:50:54 +0900

    [Gold V] Title: 로봇 청소기, Time: 108 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -2,77 +2,60 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int[][] map;
-	static boolean[][] visited;
 	static int N, M;
+	static int[] dr = new int[] {-1, 0, 1, 0};
+	static int[] dc = new int[] {0, 1, 0, -1};
+	static int[][] map;
 	static int result = 0;
-
-	// 북, 동, 남, 서
-	static int[] dr = new int[]{-1, 0, 1, 0};
-	static int[] dc = new int[]{0, 1, 0, -1};
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
+		map = new int[N][M];
 
-		// 로봇 청소기
 		st = new StringTokenizer(br.readLine());
-		int nowR = Integer.parseInt(st.nextToken());
-		int nowC = Integer.parseInt(st.nextToken());
+		int curR = Integer.parseInt(st.nextToken());
+		int curC = Integer.parseInt(st.nextToken());
 		int dir = Integer.parseInt(st.nextToken());
 
-		// 방 정보
-		visited = new boolean[N][M];
-		map = new int[N][M];
-		for(int i = 0; i < N; i++) {
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j = 0; j < M; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 
-		DFS(nowR, nowC, dir);
-		System.out.println(result);
+		clean(curR, curC, dir);
+		System.out.print(result);
 	}
 
-	static void DFS(int nowR, int nowC, int dir) {
-		if(!visited[nowR][nowC]) {
-			// 현재 칸 청소
-			visited[nowR][nowC] = true;
+	static void clean(int curR, int curC, int dir) {
+		if (map[curR][curC] == 0) {
+			map[curR][curC] = 2; // 청소한 칸은 2
 			result++;
 		}
-
-
-
-		// 주변 4칸에 청소 안한 칸 있는지 탐색
+		
 		int tempDir = dir;
 		for(int i = 0; i < 4; i++) {
-			// 반시계 방향 회전
 			tempDir = (tempDir + 3) % 4;
-			int nextR = nowR + dr[tempDir];
-			int nextC = nowC + dc[tempDir];
-
-			if(nextR >= 0 && nextR < N && nextC >= 0 && nextC < M) {
-				if(!visited[nextR][nextC] && map[nextR][nextC] == 0) {
-					DFS(nextR, nextC, tempDir);
+			int nextR = curR + dr[tempDir];
+			int nextC = curC + dc[tempDir];
+			if (nextR >= 0 && nextR < N && nextC >= 0 && nextC < M) { // 범위 체크
+				if(map[nextR][nextC] == 0) { // 청소되지 않았을 경우
+					clean(nextR, nextC, tempDir);
 					return;
 				}
 			}
 		}
-
-		// 주변 모두 청소된 경우
-		// 현재 방향에서 후진
-		int nextR = nowR - dr[dir];
-		int nextC = nowC - dc[dir];
-
-		if(nextR >= 0 && nextR < N && nextC >= 0 && nextC < M) {
-			if(map[nextR][nextC] == 0) {
-				DFS(nextR, nextC, dir);
+		
+		int nextR = curR + dr[(dir + 2) % 4];
+		int nextC = curC + dc[(dir + 2) % 4];
+		if (nextR >= 0 && nextR < N && nextC >= 0 && nextC < M) { // 범위 체크
+			if(map[nextR][nextC] != 1) { // 후진 가능한 경우
+				clean(nextR, nextC, dir); // 현재 방향 그대로 후진
 			}
 		}
-
 	}
 }
