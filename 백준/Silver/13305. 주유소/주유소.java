488a7c1d012fbc0bdc01e1a2a97d5b9f211be5a1
Yujin Park <101379241+u-genuine@users.noreply.github.com>
2025-11-05 15:51:54 +0900

    [Silver III] Title: 주유소, Time: 384 ms, Memory: 35996 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -6,23 +6,22 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        int[] distance = new int[N]; // 1 ~ N-1 바로 다음 도시까지 거리
+        long[] distance = new long[N]; // 1 ~ N-1 바로 다음 도시까지 거리
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 1; i < N; i++) {
-            distance[i] = Integer.parseInt(st.nextToken());
+            distance[i] = Long.parseLong(st.nextToken());
         }
         
-        int[] ppl = new int[N]; // 1 ~ N-1
+        long[] ppl = new long[N]; // 1 ~ N-1
         st = new StringTokenizer(br.readLine());
         for(int i = 1; i < N; i++) {
-            ppl[i] = Integer.parseInt(st.nextToken());
+            ppl[i] = Long.parseLong(st.nextToken());
         }
 
-        // 초기 값
-        int min = ppl[1];
-        int total = min * distance[1];
+        long min = Integer.MAX_VALUE;
+        long total = 0;
         
-        for(int i = 2; i < N; i++) {
+        for(int i = 1; i < N; i++) {
             if(min >= ppl[i]) {
                 min = ppl[i];
                 total += ppl[i] * distance[i];
